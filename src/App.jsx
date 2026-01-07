5b1223670b4796b20bdd0143bc675461c1868d12
Christophe-THEVENET <christophethevenet2.0@gmail.com>
2026-01-07 17:10:29 +0100

    feat: add header desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Navbar } from "@/layout/Navbar"
+import { Hero } from "@/sections/Hero"
+import { About } from "@/sections/About"
+import { Services } from "@/sections/Services"
+import { Projects } from "@/sections/Projects"
+import { Contact } from "@/sections/Contact"
 
 function App() {
 
     return (
-        <>
-        </>
+        <div className="min-h-screen overflow-x-hidden">
+            <Navbar />
+            <main>
+                <Hero />
+                <About />
+                <Services />
+                <Projects />
+                <Contact />
+            </main>
+        </div>
     )
 }
 
